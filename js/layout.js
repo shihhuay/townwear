9b9280f5f2ee7ff6271010b4275f95ac22618d26
Shih-Hua Yu <shihhuay@gmail.com>
2018-01-16 01:39:11 -0500

    cleaned layout.css and layout.js and fixed index.html

--- js/layout.js
+++ js/layout.js
@@ -56,27 +56,6 @@ var Layout = function () {
 
     // handle carousel
     var handleCarousel = function() {
-    /*
-        var $item = $('.carousel .item'); 
-        var $wHeight = $(window).height();
-        $item.eq(0).addClass('active');
-        $item.height($wHeight); 
-        $item.addClass('full-screen');
-
-        $('.carousel img').each(function() {
-            var $src = $(this).attr('src');
-            var $color = $(this).attr('data-color');
-            $(this).parent().css({
-                'background-image' : 'url(' + $src + ')',
-                'background-color' : $color
-            });
-            $(this).remove();
-        });
-
-        $(window).on('resize', function (){
-            $wHeight = $(window).height();
-            $item.height($wHeight);
-        });*/
         
         //adjust to screen width
         
